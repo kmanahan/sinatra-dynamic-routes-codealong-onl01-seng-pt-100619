b0ec94c90239e5ce408382ea35d882c23e440e09
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-01-23 19:17:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,8 +20,9 @@ class App < Sinatra::Base
   end 
 
   get '/mulitply' do 
-    @product = params[:num1 * :num2]
-    "#{@product}"
+    @num1 = params[:num1]
+    @num2 = params[:num2]
+    "#{@num1.to_i * @num2.to_i }"
     
   end 
 end
